d98cf17877eb02978af3de5e2cc5b17999e7e97a
Amy-S-Anderson <anderson.amy.susan@gmail.com>
2026-06-19 11:21:17 -0600

    added year_born column to age-structured starting population (NA for time0 agents, unless age == 0; then year_born = 0)

--- R/create_starting_population.R
+++ R/create_starting_population.R
@@ -27,7 +27,8 @@ create_pop <- function(pop0_size, age_structured,
     pop0 <- create_pop_stable_age(pop0_size = pop0_size,
                                   mortality_regime = mortality_regime,
                                   r       = r, # pop. growth rate
-                                  max_age = 100)
+                                  max_age = 100,
+                                  year_born = NA_real_)
   }
   
   if(age_structured == FALSE){
